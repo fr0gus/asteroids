1c083bc4542c57a15e98b1fe1fe4a8ca446dd9fa
fr0gus <claudiu.manole@outlook.com>
2025-09-07 19:56:36 +0200

    Update: Introduced clock and dt

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +19,7 @@ def main():
 
         
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
